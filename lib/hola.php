66361b1cadd2830c4b2e0ac0f93f75059cd69100
ivan <ivanjimenezquintero@outlook.com>
2023-10-17 10:03:58 +0200

    Programa interactivo

--- lib/hola.php
+++ lib/hola.php
@@ -1,6 +1,6 @@
 <?php
 //Autor: Ivan <ijimqui3008@iestrassierra.es>
-//El nombre original es Mundo
-$nombre = isset ($argv[1])?$argv[1]:"Mundo";
-@print "Hola{$argb[1]}\n";
+print "Introduzce tu nombre";
+$nombre =trim(fgets(STDIN));
+@print "Hola{$nombre}\n";
 ?>
